0065fd8ab9c53000c8f6942c7e646fbded688ad4
KyerrreN <kyerrrenmgt@gmail.com>
2024-09-21 20:45:49 +0300

    feat: add initial footer

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "../Wrapper/Wrapper.css";
 
+// Import components
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function Wrapper() {
     return (
         <div className="wrapper">
             <Header />
+            <Footer />
         </div>
     );
 }
