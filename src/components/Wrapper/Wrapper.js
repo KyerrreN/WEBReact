d417a8454f4309bbfdcb7f3aeabfb932dee44630
KyerrreN <kyerrrenmgt@gmail.com>
2024-09-21 21:35:21 +0300

    feat: add about us section in index page

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -4,11 +4,13 @@ import "../Wrapper/Wrapper.css";
 // Import components
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import IndexAbout from "../IndexAbout/IndexAbout";
 
 function Wrapper() {
     return (
         <div className="wrapper">
             <Header />
+            <IndexAbout />
             <Footer />
         </div>
     );
