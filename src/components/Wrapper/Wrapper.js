2e78cacb5f4237a693d6621591260c44e7f6a94a
KyerrreN <kyerrrenmgt@gmail.com>
2024-09-21 11:53:33 +0300

    feat: add wrapper to an app
    Added wrapper to wrap web application for an adaptive layout

---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../Wrapper/Wrapper.css";
+
+class Wrapper extends Component {
+    render() {
+        return <div className="wrapper"></div>;
+    }
+}
+
+export default Wrapper;
