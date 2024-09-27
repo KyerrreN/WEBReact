e224dfc32399dbec6cddd8987e0274998d91b94a
KyerrreN <kyerrrenmgt@gmail.com>
2024-09-27 15:39:54 +0300

    feat: add partnership section in partners page

--- src/pages/Partners.jsx
+++ src/pages/Partners.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import BurgerMenu from "../components/BurgerMenu/BurgerMenu";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import PartnersMain from "../components/PartnersMain/PartnersMain";
 
 export default function Partners() {
     return (
         <>
             <BurgerMenu />
             <Header />
+            <PartnersMain />
             <Footer />
         </>
     );
