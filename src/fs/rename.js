0fe225f1a39b4bfa0956cec1e438fdc4eb7a6400
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-01 23:23:07 +0300

    feat: add rename function

---
+++ src/fs/rename.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+
+function rename(oldPath, newPath) {
+    if (!fs.existsSync(oldPath)) {
+        console.error("Provided source doesn't exist");
+        return;
+    }
+
+    if (!fs.statSync(oldPath).isFile()) {
+        console.error("Provided source is correct, but it is not a file");
+        return;
+    }
+
+    const dir1 = path.dirname(oldPath);
+    const dir2 = path.dirname(newPath);
+
+    if (path.normalize(dir1) !== path.normalize(dir2)) {
+        console.error("Provided old file name doesnt equal new file name");
+        return;
+    }
+
+    fs.renameSync(oldPath, newPath);
+    console.log(
+        'Succesfully changed name from "' +
+            path.basename(oldPath) +
+            '" to "' +
+            path.basename(newPath) +
+            '"'
+    );
+}
+
+// rename("./freelancers/f-1.txt", "./freelancers/f-1.json");
+
+const [oldPathName, newPathName] = process.argv.slice(2);
+
+if (oldPathName && newPathName) {
+    rename(oldPathName, newPathName);
+} else {
+    console.error(
+        'The arguments for rename function are as follows\n"old_path" "new_path"'
+    );
+}
