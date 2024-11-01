68be99e4bcb259cfb1aadf14a801f205be1f36ce
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-01 23:57:49 +0300

    fix: seeding the correct type of id

--- src/fs/_seed.js
+++ src/fs/_seed.js
@@ -10,8 +10,8 @@ try {
     });
 } catch (e) {}
 
-createFreelancersEntry("1", "Vadim", "Podlipny", "Web Developer", 4.2);
-createFreelancersEntry("2", "Vadim", "Podlipny", "Web Developer", 4.3);
-createFreelancersEntry("3", "Vadim", "Podlipny", "Web Developer", 4.4);
-createFreelancersEntry("4", "Vadim", "Podlipny", "Web Developer", 4.5);
-createFreelancersEntry("5", "Vadim", "Podlipny", "Web Developer", 4.6);
+createFreelancersEntry(1, "Vadim", "Podlipny", "Web Developer", 4.2);
+createFreelancersEntry(2, "Vadim", "Podlipny", "Web Developer", 4.3);
+createFreelancersEntry(3, "Vadim", "Podlipny", "Web Developer", 4.4);
+createFreelancersEntry(4, "Vadim", "Podlipny", "Web Developer", 4.5);
+createFreelancersEntry(5, "Vadim", "Podlipny", "Web Developer", 4.6);
