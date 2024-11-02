7a2b08090a3b934a2e6b89e6128ca68a683b2c98
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-02 21:36:41 +0300

    fix: write stream function now accepts file name as an argument

--- src/streams/write.js
+++ src/streams/write.js
@@ -2,136 +2,131 @@ const fs = require("fs");
 const path = require("path");
 
 function writeNewFreelancers(data) {
-    let dataArray;
-    try {
-        dataArray = JSON.parse(data);
-    } catch (e) {
-        console.error("Your data is not in JSON format");
+    const pathToNewFreelancer = path.join(__dirname, "freelancers", data);
+
+    if (!fs.existsSync(pathToNewFreelancer)) {
+        console.error("Error: file with provided name doesn't exist");
         return;
     }
 
-    if (Array.isArray(dataArray)) {
-        const isValidJson = validateArray(dataArray);
+    if (!fs.statSync(pathToNewFreelancer).isFile()) {
+        console.error("Error: provided path is not a file");
+        return;
+    }
 
-        if (!isValidJson) {
+    const firstReadStream = fs.createReadStream(pathToNewFreelancer, {
+        encoding: "utf-8",
+        highWaterMark: 2 * 1024,
+    });
+
+    let fileContents = "";
+
+    firstReadStream.on("data", (chunk) => {
+        fileContents += chunk;
+    });
+
+    firstReadStream.on("error", (err) => {
+        console.error("Error reading a file. Error message: " + err);
+    });
+
+    firstReadStream.on("end", () => {
+        let dataArray;
+        try {
+            dataArray = JSON.parse(fileContents);
+        } catch (e) {
+            console.error("Your data is not in JSON format");
             return;
         }
-    } else if (typeof dataArray === "object" && dataArray !== null) {
-        const isValidJson = validateObject(dataArray);
 
-        if (!isValidJson) {
+        if (Array.isArray(dataArray)) {
+            const isValidJson = validateArray(dataArray);
+
+            if (!isValidJson) {
+                return;
+            }
+        } else if (typeof dataArray === "object" && dataArray !== null) {
+            const isValidJson = validateObject(dataArray);
+
+            if (!isValidJson) {
+                return;
+            }
+        } else {
+            console.error("Parsed data is neither an array nor an object");
             return;
         }
-    } else {
-        console.error("Parsed data is neither an array nor an object");
-        return;
-    }
 
-    if (!fs.existsSync(path.join(__dirname, "Freelancers", "f-index.json"))) {
-        console.error("Error: f-index.json doesnt exist");
-        return;
-    }
+        if (
+            !fs.existsSync(path.join(__dirname, "Freelancers", "f-index.json"))
+        ) {
+            console.error("Error: f-index.json doesnt exist");
+            return;
+        }
 
-    const indexDataText = fs.readFileSync(
-        path.join(__dirname, "Freelancers", "f-index.json"),
-        "utf-8"
-    );
+        const indexDataText = fs.readFileSync(
+            path.join(__dirname, "Freelancers", "f-index.json"),
+            "utf-8"
+        );
 
-    let indexArray;
-    try {
-        indexArray = JSON.parse(indexDataText);
-    } catch (e) {
-        console.error("Error parsing f-index.json");
-        return;
-    }
+        let indexArray;
+        try {
+            indexArray = JSON.parse(indexDataText);
+        } catch (e) {
+            console.error("Error parsing f-index.json");
+            return;
+        }
 
-    if (Array.isArray(dataArray)) {
-        indexArray = indexArray.filter((el) => {
-            return !dataArray.some((anotherEl) => el.id === anotherEl.id);
-        });
+        if (Array.isArray(dataArray)) {
+            indexArray = indexArray.filter((el) => {
+                return !dataArray.some((anotherEl) => el.id === anotherEl.id);
+            });
 
-        indexArray.push(...dataArray);
+            indexArray.push(...dataArray);
 
-        dataArray.forEach((d) => {
+            dataArray.forEach((d) => {
+                const writeStream = fs.createWriteStream(
+                    path.join(__dirname, "freelancers", "f-" + d.id + ".json"),
+                    { flags: "w", encoding: "utf-8" }
+                );
+
+                writeStream.write(JSON.stringify(d, null, 2));
+
+                writeStream.end(() => {
+                    console.log("Finished writing data to f-" + d.id + ".json");
+                });
+            });
+
             const writeStream = fs.createWriteStream(
-                path.join(__dirname, "freelancers", "f-" + d.id + ".json"),
+                path.join(__dirname, "freelancers", "f-index.json"),
                 { flags: "w", encoding: "utf-8" }
             );
 
-            writeStream.write(JSON.stringify(d, null, 2));
-
-            writeStream.end(() => {
-                console.log("Finished writing data to f-" + d.id + ".json");
+            writeStream.write(JSON.stringify(indexArray, null, 2));
+        } else {
+            indexArray = indexArray.filter((el) => {
+                return el.id !== dataArray.id;
             });
-        });
 
-        const writeStream = fs.createWriteStream(
-            path.join(__dirname, "freelancers", "f-index.json"),
-            { flags: "w", encoding: "utf-8" }
-        );
+            indexArray.push(dataArray);
 
-        writeStream.write(JSON.stringify(indexArray, null, 2));
-    } else {
-        indexArray = indexArray.filter((el) => {
-            return el.id !== dataArray.id;
-        });
+            const writeStreamIndex = fs.createWriteStream(
+                path.join(__dirname, "freelancers", "f-index.json"),
+                { flags: "w", encoding: "utf-8" }
+            );
 
-        indexArray.push(dataArray);
+            const writeStreamData = fs.createWriteStream(
+                path.join(
+                    __dirname,
+                    "freelancers",
+                    "f-" + dataArray.id + ".json"
+                ),
+                { flags: "w", encoding: "utf-8" }
+            );
 
-        const writeStreamIndex = fs.createWriteStream(
-            path.join(__dirname, "freelancers", "f-index.json"),
-            { flags: "w", encoding: "utf-8" }
-        );
-
-        const writeStreamData = fs.createWriteStream(
-            path.join(__dirname, "freelancers", "f-" + dataArray.id + ".json"),
-            { flags: "w", encoding: "utf-8" }
-        );
-
-        writeStreamIndex.write(JSON.stringify(indexArray, null, 2));
-        writeStreamData.write(JSON.stringify(dataArray, null, 2));
-    }
+            writeStreamIndex.write(JSON.stringify(indexArray, null, 2));
+            writeStreamData.write(JSON.stringify(dataArray, null, 2));
+        }
+    });
 }
-
-const data = [
-    {
-        id: 1,
-        name: "1",
-        surname: "1",
-        spec: "1",
-        rating: 1,
-    },
-    {
-        id: 2,
-        name: "1",
-        surname: "1",
-        spec: "1",
-        rating: 1,
-    },
-    {
-        id: 3,
-        name: "1",
-        surname: "1",
-        spec: "1",
-        rating: 1,
-    },
-    {
-        id: 8,
-        name: "1",
-        surname: "1",
-        spec: "1",
-        rating: 1,
-    },
-    {
-        id: 9,
-        name: "1",
-        surname: "1",
-        spec: "1",
-        rating: 1,
-    },
-];
-
-writeNewFreelancers(JSON.stringify(data, null, 2));
 
 function validateArray(array) {
     for (let i = 0; i < array.length; i++) {
@@ -164,4 +159,12 @@ function validateObject(obj) {
     });
 
     return true;
+}
+
+const [pathToJson] = process.argv.slice(2);
+
+if (pathToJson) {
+    writeNewFreelancers(pathToJson);
+} else {
+    console.error("Please specify a file name");
 }
