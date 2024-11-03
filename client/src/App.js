efdd28eedf19e04e309c10042c69646655aa2b5f
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-03 19:53:23 +0300

    feat: add get request to send json data

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,45 @@
 import React from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
-    return <div>Hello world</div>;
+    const [freelancers, setFreelancers] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/freelancers")
+            .then((response) => response.json())
+            .then((data) => {
+                setFreelancers(data);
+            })
+            .catch((error) =>
+                console.log("There was a problem fetching: " + error)
+            );
+    }, []);
+
+    return (
+        <div className="wrapper">
+            <div className="container">
+                {typeof freelancers === "undefined" ? (
+                    <p>Loading</p>
+                ) : freelancers.length === 0 ? (
+                    <p>There are no freelancers in the DB</p>
+                ) : (
+                    freelancers.map((freel, i) => {
+                        return (
+                            <>
+                                <span>Freelancer {freel.id}</span>
+                                <span>
+                                    Name: {freel.name} {freel.surname}
+                                </span>
+                                <span>Specialty: {freel.spec}</span>
+                                <span style={{ paddingBottom: 20 }}>
+                                    Rating: {freel.rating}
+                                </span>
+                            </>
+                        );
+                    })
+                )}
+            </div>
+        </div>
+    );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
+const cors = require("cors");
+
+function getFreelancers() {
+    const pathToFile = path.join(__dirname, "data", "f.json");
+    const data = fs.readFileSync(pathToFile);
+    return JSON.parse(data);
+}
 
 // usestaticfiles
 const publicPath = path.join(__dirname, "..", "client", "public");
 app.use(express.static(publicPath));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 
+//get requests
+app.get("/api/freelancers", (req, res) => {
+    const data = getFreelancers();
+    res.status(200).json(data);
+});
 app.get("/", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
 });
